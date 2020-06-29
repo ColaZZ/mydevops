4b999fc9a113a8d4bb1e07e9dddf64ff0cd29636
cheung <zlinxx@foxmail.com>
2020-06-29 17:29:22 +0800

    add command_line argv

--- manage.py
+++ manage.py
@@ -4,18 +4,30 @@ import os
 import sys
 
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydevops.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+# def main():
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydevops.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 3:
+        run_arg = sys.argv[2]
+        if not run_arg.startswith('--settings'):
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings.settings")
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
